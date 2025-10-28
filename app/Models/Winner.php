6fdab440c0833ca3c65713eea008dfc0e130ad57
Mir Shifat Mahmud <mir19438.bdcallingit@gmail.com>
2025-10-28 18:02:24 +0600

    model fillable

--- app/Models/Winner.php
+++ app/Models/Winner.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Winner extends Model
 {
-    //
+    use HasFactory;
+
+    protected $guarded = ['id'];
 }
